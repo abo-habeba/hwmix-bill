1dd04e080d60be20932bd9fd024c3c51c42f4168
heshame <atsh@gmail.com>
2025-07-03 11:57:36 +0300

    تعديل دوال جلب البيانات

---
+++ dist/assets/attributeValues-14nJCc2S.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-CKIt4_Zz.js";const r={};function o(a,n){return c(),t("div")}const _=e(r,[["render",o]]);export{_ as default};
---
+++ dist/assets/inventory-adjustments-B5GZdx4P.js
@@ -0,0 +1,1 @@
+import{c as e,o as t}from"./index-CKIt4_Zz.js";const c={__name:"inventory-adjustments",setup(n){return(o,r)=>(t(),e("div",null,"قائمة تسويات المخزون"))}};export{c as default};
