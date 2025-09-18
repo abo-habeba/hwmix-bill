961b2eb96c521a8957df81fb1dc6554fae698f09
hesham <atsh.phone@gmail.com>
2025-09-18 22:31:25 +0300

    run build

--- dist/assets/installments-JZ17cA9s.js
+++
@@ -1,1 +0,0 @@
-import{I as a}from"./InstallmentsDataTable-CYuXsfUh.js";import{k as t,j as e,U as s,o as r,d as o}from"./index-B6FT7dz_.js";import"./PayInstallmentDialog-DrhPSZWP.js";const i={__name:"installments",setup(l){return(c,n)=>(r(),t(s,{class:"pa-0",title:"الاقساط"},{default:e(()=>[o(a)]),_:1}))}};export{i as default};
---
+++ dist/assets/payments-e8wAiDZX.js
@@ -0,0 +1,1 @@
+import{c as e,o as t}from"./index-Hb9LFdW-.js";const r={__name:"payments",setup(a){return(c,n)=>(t(),e("div",null,"قائمة سندات الصرف"))}};export{r as default};
